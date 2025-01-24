761c58a46e4da5bc063874b9cf1a1dbea66052b3
Pedro Ferreira <pedro.leandro@sankhya.com.br>
2025-01-23 21:44:27 -0300

    feat: Acessando e Validando tilulo do pagina html

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,7 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título da aplicação', () => {
+    cy.visit('src/index.html');
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
+
+  })
+})
