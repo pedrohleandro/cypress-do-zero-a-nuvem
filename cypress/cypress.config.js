22ba75cbddd4e34dd26d21078f5a70ba6a8f4a64
Pedro Ferreira <pedro.leandro@sankhya.com.br>
2025-03-08 21:46:12 -0300

    Configure cypress cloud

--- cypress/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  projectId: "eg43tm",
-  viewportHeight: 880,
-  viewportWidth: 1280,
-  e2e: {},
-  // video: true
-})
